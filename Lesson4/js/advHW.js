75e0bd0ecf6f88374f04c4d35e06b885c6e7b20d
lapotko <lapotko.d@gmail.com>
2022-04-08 21:49:47 +0300

    L4. Has made draft non-optimized version of form verification

--- Lesson4/js/advHW.js
+++ Lesson4/js/advHW.js
@@ -1,13 +1,24 @@
-let nameInput = document.getElementById('name');
+
 let button = document.querySelector('input[type="submit"]');
 let namePattern = /^[а-яА-яЁёa-zA-Z]*?\s?[а-яА-яЁёa-zA-Z]*[\-]?[а-яА-яЁёa-zA-Z]*\s?$/gmi;
 let phonePattern = /^\+7\(\d{3}\)\d{3}-\d{4}$/;
 let mailPattern = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$/gmi
 button.onclick = function () {
-    let str = nameInput.value;
-    if (!namePattern.test(str)) {
+    /* let nameInput = document.getElementById('name');
+    let phoneInput = document.getElementById('phone');
+    let mailInput = document.getElementById('mail');
+    let nameStr = nameInput.value;
+    if (!namePattern.test(nameStr)) {
         console.log('error');
     } else console.log('ok');
+    let phoneStr = phoneInput.value;
+    if (!phonePattern.test(phoneStr)) {
+        console.log('error');
+    } else console.log('ok');
+    let mailStr = mailInput.value;
+    if (!mailPattern.test(mailStr)) {
+        console.log('error');
+    } else console.log('ok'); */
 }
 //if input[i].id.test
 //it`s better do this with "forEach" method

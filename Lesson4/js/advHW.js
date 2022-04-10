d9f0cdb736029efd2deea09f5b81d2272d2c0bca
Denis Lapotko <lapotko.d@gmail.com>
2022-04-10 23:10:11 +0300

    Добавил валидацию формы HTML5

--- Lesson4/js/advHW.js
+++ Lesson4/js/advHW.js
@@ -1,35 +1,28 @@
 
 let button = document.querySelector('button[type="submit"]');
 let patterns = {
+    //свойстов должно совпадать с id у input'а
     name: /^[а-яА-яЁёa-zA-Z]+\s?[а-яА-яЁёa-zA-Z]*[\-]?[а-яА-яЁёa-zA-Z]*\s?$/gmi,
     phone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
     mail: /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$/gmi
 }
-
-button.onclick = function (event) {
+function test(event) {
     let inputs = Array.from(document.getElementsByTagName('input'));//make array from collection
     inputs.forEach(element => {
+        let p = element.nextElementSibling;
         let pattern = patterns[element.id];
         if (!pattern.test(element.value)) {
             event.preventDefault();
-            console.log('preventDefault');
+            console.log('Не валидно = ' + element.value);
+            element.classList.add('error');
+            p.classList.add('visible');
+            element.value = '';
+        } else {
+            p.classList.remove('visible');
+            element.classList.remove('error');
+            console.log('Данные приняты');
         }
     })
-    //How can I get value from each input of collection with no need to create variable obviously for each of them;
-    /* let nameInput = document.getElementById('name');
-    let phoneInput = document.getElementById('phone');
-    let mailInput = document.getElementById('mail');
-    let nameStr = nameInput.value;
-    if (!namePattern.test(nameStr)) {
-        console.log('error');
-    } else console.log('ok');
-    let phoneStr = phoneInput.value;
-    if (!phonePattern.test(phoneStr)) {
-        console.log('error');
-    } else console.log('ok');
-    let mailStr = mailInput.value;
-    if (!mailPattern.test(mailStr)) {
-        console.log('error');
-    } else console.log('ok'); */
 }
+button.onclick = test;
 

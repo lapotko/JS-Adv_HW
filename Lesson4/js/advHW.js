b534c5933fe3203fe2ff5fbd5785c861bd742b88
Denis Lapotko <lapotko.d@gmail.com>
2022-04-11 20:51:45 +0300

    final commit with some issues to fix in future

--- Lesson4/js/advHW.js
+++ Lesson4/js/advHW.js
@@ -2,9 +2,9 @@
 let button = document.querySelector('button[type="submit"]');
 let patterns = {
     //свойстов должно совпадать с id у input'а
-    name: /^[а-яА-яЁёa-zA-Z]+\s?[а-яА-яЁёa-zA-Z]*[\-]?[а-яА-яЁёa-zA-Z]*\s?$/gmi,
+    name: /^[а-яА-яЁёa-zA-Z]+\s?[а-яА-яЁёa-zA-Z]*[\-]?[а-яА-яЁёa-zA-Z]*\s?$/gi,
     phone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
-    mail: /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$/gmi
+    mail: /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$/gi
 }
 function test(event) {
     let inputs = Array.from(document.getElementsByTagName('input'));//make array from collection
@@ -20,7 +20,7 @@ function test(event) {
         } else {
             p.classList.remove('visible');
             element.classList.remove('error');
-            console.log('Данные приняты');
+            alert('Данные приняты');
         }
     })
 }
